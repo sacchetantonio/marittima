b4752b5501d4e88a11d462c97c7a9ddf6c0207db
sacchetantonio <antonio.sacchet@mail.polimi.it>
2022-05-12 16:22:40 +0200

    top

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-let express = require('express');
+let express = require("express");
 
 let app = express();
 
-let port = 3000;
+let port = process.env.PORT || 3000;
 
 let server = app.listen(port);
+app.use(express.static("public"));
 
 console.log("running server on http://localhost:" + port);
